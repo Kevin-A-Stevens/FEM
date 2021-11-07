5da849f35ca30d2709383ad62f039924803c4779
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2021-11-07 15:17:00 -0600

    javascript loops

--- Day5/js/script.js
+++ Day5/js/script.js
@@ -34,9 +34,85 @@ let isTheSkyBlue = true;
 console.log(isTheSkyBlue);
 
 // numbers
-
 const num = 5;
 // Largest integer you can have
 console.log(Number.MAX_SAFE_INTEGER);
 // Smallest integer you can have
 console.log(Number.MIN_SAFE_INTEGER);
+
+// Control Flow
+const isSkyBlue = true;
+if (isSkyBlue) {
+  console.log("The sky is blue");
+} else {
+  console.log("The sky is not blue");
+}
+
+const myVariable = 10;
+if (2 + 2 === 4) {
+  console.log("Hppray! Math still works");
+} else {
+  console.log("Panic!");
+}
+
+// else if
+const friendsAtYourParty = 4;
+if (friendsAtYourParty === 0) {
+  console.log("Cool, I have a lot of food to myself");
+} else if (friendsAtYourParty <= 4) {
+  console.log("perfect amount to play Mario Kart");
+} else {
+  console.log("Wow! Play the dance music");
+}
+
+//Loops
+console.log("LOOPS");
+let myFriends = 0;
+
+// While loop
+while (myFriends < 10) {
+  myFriends = myFriends + 1;
+  //   myFriends += 1; same as above
+  //   myFriends++; same as above
+}
+
+console.log(myFriends);
+
+// for loop
+for (let i = 0; i <= 10; i++) {
+  myFriends++;
+}
+
+console.log(myFriends);
+
+const character = "a";
+const timesToRepeat = 5;
+let myString = "";
+
+for (let i = 0; i < timesToRepeat; i++) {
+  myString += character;
+}
+
+console.log(myString);
+
+// Functions
+console.log("FUNCTIONS");
+// function name(input) {  -- function name takes an input
+//    return input + 2;  -- returns an output
+// }
+
+function addTwo(number) {
+  return number + 2;
+}
+
+const finalAnswer = addTwo(5);
+const finalAnswer2 = addTwo(10);
+console.log(finalAnswer);
+console.log(finalAnswer2);
+
+// Scope
+// Every time you call a function, it has its own scope
+// Variables defines inside a function are thrown out once that function ends
+// Variables defined outside of a function are global and can be used by functions
+// This is the same for loops - let i = 0 is only in scope for the for loop
+// Where a variable is declared matters
