9f251ce94acfbce12b0bb4768fd2186f55e5d1ad
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2021-11-13 11:09:11 -0600

    Finish functions for Math

--- Day4/js/calculator.js
+++ Day4/js/calculator.js
@@ -8,11 +8,12 @@ let runningTotal = 0;
 
 let buffer = "0"; // Keep track of what is on screen
 
-let previousOperator; // represent what operator they clicked last (+ - / *)
+let previousOperator = null; // represent what operator they clicked last (+ - / *)
 
 const screen = document.querySelector(".screen"); // What we will be writing out to
 
 function buttonClick(value) {
+  //   debugger;
   if (isNaN(value)) {
     // this is not a number
     handleSymbol(value);
@@ -24,9 +25,81 @@ function buttonClick(value) {
 }
 
 function handleSymbol(symbol) {
-  if (symbol === "C") {
-    buffer = "0";
-    runningTotal = "0";
+  switch (symbol) {
+    case "C":
+      buffer = "0";
+      runningTotal = "0";
+      break;
+    case "=":
+      if (previousOperator === null) {
+        // need two numbers to do math
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer === "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "−":
+    case "×":
+    case "÷":
+      handleMath(symbol);
+      break;
+  }
+}
+
+function handleMath(symbol) {
+  if (buffer === "0") {
+    // do nothing
+    return;
+  }
+
+  const intBuffer = parseInt(buffer); // converts number string to a number
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperator = symbol;
+  buffer = "0";
+}
+
+// function flushOperation(intBuffer) {
+//   switch (previousOperator) {
+//     case "+":
+//       runningTotal += intBuffer;
+//       break;
+//     case "−":
+//       runningTotal -= intBuffer;
+//       break;
+//     case "×":
+//       runningTotal *= intBuffer;
+//       break;
+//     case "÷":
+//       runningTotal /= intBuffer;
+//       break;
+//   }
+// }
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "−") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
   }
 }
 
