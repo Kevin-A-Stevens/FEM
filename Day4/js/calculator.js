2e0b021467f50aac5b3c359623ddfa1c679ff98b
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2021-11-13 09:25:54 -0600

    Set up functions and code init function

--- Day4/js/calculator.js
+++ Day4/js/calculator.js
@@ -1,2 +1,44 @@
 // Writing many small functions are preferable to one large function
 // A function should do one thing and do it well
+
+// Keep source of truth out of the DOM and in JS
+
+// Keep track of numbers
+let runningTotal = 0;
+
+let buffer = "0";
+
+let previousOperator; // represent what operator they clicked last (+ - / *)
+
+const screen = document.querySelector(".screen"); // What we will be writing out to
+
+function buttonClick(value) {
+  if (isNaN(value)) {
+    // this is not a number
+    handleSymbol(value);
+  } else {
+    // This is a number
+    handleNumber(value);
+  }
+}
+
+function handleSymbol(symbol) {}
+
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+}
+
+function init() {
+  document
+    .querySelector(".calc-buttons") // grabs the section with class of calc-buttons
+    .addEventListener("click", function (event) {
+      // on click event, run this function
+      buttonClick(event.target.innerText);
+    });
+}
+
+init();
