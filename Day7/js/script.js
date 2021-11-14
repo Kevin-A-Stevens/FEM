d73f0630376cdb0dcb6bee1001afc216158f0846
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2021-11-13 21:52:37 -0600

    learning popmotion and swiper

--- Day7/js/script.js
+++ Day7/js/script.js
@@ -1,6 +1,9 @@
 new Swiper(".swiper", {
   speed: 400,
   spaceBetween: 100,
+  effect: "slide", // flip cube fade slide creative cards coverflow
+  slidesPerView: 1, // default is 1
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -28,3 +31,27 @@ new Swiper(".swiper", {
 //       el: '.swiper-scrollbar',
 //     },
 //   });
+
+// Navigation Physics
+
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector(".brand");
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, "mousedown touchstart").start((e) => {
+  e.preventDefault();
+  pointer(ballXY.get()).start(ballXY);
+});
+
+listen(document, "mouseup touchend").start(() => {
+  spring({
+    from: ballXY.get(),
+    velocity: ballXY.getVelocity(),
+    to: { x: 0, y: 0 },
+    stiffness: 200,
+    mass: 1,
+    damping: 10,
+  }).start(ballXY);
+});
